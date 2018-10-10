2229ad8b894dc507d164110eaeda13c711c73c2e
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-10 14:34:03 -0300

    Explicita os tempos e insere labels em todos os forms.
    Os labels permitem que os nomes efetivos dos forms sejam usados em vez
    de seus nomes originais. "endereco_origem" fica "endereço de origem",
    por exemplo.

--- solicitacao/forms.py
+++ solicitacao/forms.py
@@ -1,14 +1,30 @@
-from django import forms
+from django import forms  # from django.forms import ModelForm
 from .models import Empresa, Solicitacao
+from django.utils.translation import gettext_lazy as _
 
 
 class EmpresaForm(forms.ModelForm):
     class Meta:
         model = Empresa
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('nome', 'cnpj')
+        labels = {
+            'nome': _('Nome'),
+            'cnpj': _('CNPJ'),
+        }
 
 
 class MotoboyForm(forms.ModelForm):
     class Meta:
         model = Solicitacao
         fields = '__all__'
+        #  Campos: data, solicitante, endereco_origem, contato_origem,
+        # servico, endereco_destino, contato_destino, observacoes
+        labels = {
+            'endereco_origem': _('Endereço de origem'),
+            'contato_origem': _('Contato de origem'),
+            'servico': _('Serviço'),
+            'endereco_destino': _('Endereço de destino'),
+            'contato_destino': _('Contato de destino'),
+            'observacoes': _('Observações'),
+        }
