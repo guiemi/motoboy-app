dae0bbed5193ce0cecc4e306ab5194368bc38546
guiemi <guiemi.teixeira@gmail.com>
2018-10-15 23:46:36 -0300

    WIP: query funcionando no shell; ainda não na views.py

--- solicitacao/views.py
+++ solicitacao/views.py
@@ -41,5 +41,8 @@ def solicitacao_motoboy(request):
 
 
 def detalhes_solicitacao(request, pk):
+    detalhes = Solicitacao.objects.get(pk=pk)
     context = {"pk": pk}
-    return render(request, 'detalhes_solicitacao.html', context)
+    
+    return render(request, 'detalhes_solicitacao.html', context, {'detalhes': detalhes})
+
