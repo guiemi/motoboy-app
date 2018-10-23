38934b3e3d115cf10370a9190d1feb7d8ad80cf6
Guilherme Teixeira <guilherme@vert-capital.com>
2018-10-23 18:15:55 -0300

    Remove um path que não será usado

--- motoboyapp/urls.py
+++ motoboyapp/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls import url
 app_name = 'solicitacao'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 
     path('', views.index, name='index'),
