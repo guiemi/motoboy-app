69ce2c7c0f11538b482a618fe22aaeb0c5e57077
Guilherme Teixeira <guilherme@vert-capital.com>
2018-10-26 18:13:17 -0300

    teste: trabalhando na branch master

--- solicitacao/admin.py
+++ solicitacao/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Empresa, Solicitacao
 
 admin.site.register(Empresa)
-admin.site.register(Solicitacao)
+admin.site.register(Solicitacao)  # trabalhando na branch master
